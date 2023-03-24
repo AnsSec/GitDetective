94aa44208feb63e997ef9d8b73ba341a19ffaa6b
Anshul Vishwakarma <72655193+AnsSec@users.noreply.github.com>
2023-03-24 10:44:38 +0530

    Delete lbwebdev directory

--- lbwebdev/js_basics/JSDOM 2/index.js
+++
@@ -1,9 +0,0 @@
-// document.addEventListener('click',function(){
-//     document.write("i have click on website")
-// })
-
-const content=document.querySelector('#wrapper')
-
-content.addEventListener('click',function(event){
-    console.log(event)
-})
